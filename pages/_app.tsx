dcdeb0d1a7f3de9c8d55a217f87b1881946137af
wildpow <element1028@gmail.com>
2021-06-09 15:02:12 -0700

    refactor: starting to setup snipcart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+<>
+  <Head>
+  <link rel="preconnect" href="https://app.snipcart.com"/>
+<link rel="preconnect" href="https://cdn.snipcart.com"/>
+  </Head>
+  <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
