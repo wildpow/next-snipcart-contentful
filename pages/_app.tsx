a7e586d5daf2991b0e6df825d06eb05ec3b8dc7d
wildpow <element1028@gmail.com>
2021-06-16 14:56:49 -0700

    setup test products and Snipcart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-<>
-  <Head>
-  <link rel="preconnect" href="https://app.snipcart.com"/>
-<link rel="preconnect" href="https://cdn.snipcart.com"/>
-  </Head>
-  <Component {...pageProps} />
+    <>
+      <Head>
+        <link rel="preconnect" href="https://app.snipcart.com" />
+        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <script
+          async
+          src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"
+        ></script>
+      </Head>
+
+      <Component {...pageProps} />
+      <div
+        hidden
+        id="snipcart"
+        data-api-key={process.env.NEXT_PUBLIC_SNIPCART}
+      ></div>
     </>
   )
 }
